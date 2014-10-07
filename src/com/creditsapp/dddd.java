db0c729922306ad9dfe2d1d28f22aae1b49efcc3
YarikBasuk <basyuk.yaroslav@ukr.net>
2014-10-07 11:03:29 +0300

    qsw

---
+++ src/com/creditsapp/dddd.java
@@ -0,0 +1,11 @@
+package com.creditsapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Windows
+ * Date: 07.10.14
+ * Time: 11:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class dddd {
+}
