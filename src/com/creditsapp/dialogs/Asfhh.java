42b80bed6fc83cc47251e465a8226395fd2e61ff
Alex <h>
2014-10-07 20:27:55 +0200

    pyuyguy

---
+++ src/com/creditsapp/dialogs/Asfhh.java
@@ -0,0 +1,11 @@
+package com.creditsapp.dialogs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 07.10.14
+ * Time: 20:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Asfhh {
+}
