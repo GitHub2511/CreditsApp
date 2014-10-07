eec95e5f6532bda339c7219609991e2b426b7a1f
YarikBasuk <basyuk.yaroslav@ukr.net>
2014-10-07 11:06:00 +0300

    qswdf

--- src/com/creditsapp/Test.java
+++ src/com/creditsapp/Test.java
@@ -4,8 +4,8 @@ package com.creditsapp;
  * Created with IntelliJ IDEA.
  * User: Windows
  * Date: 07.10.14
- * Time: 11:02
+ * Time: 11:05
  * To change this template use File | Settings | File Templates.
  */
-public class dddd {
+public class Test {
 }
