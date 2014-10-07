a3dfd72b0c764b54892ee6eb2fb77c43ac585ed4
YarikBasuk <basyuk.yaroslav@ukr.net>
2014-10-07 21:18:51 +0300

    dfnm

---
+++ src/com/creditsapp/Dttt.java
@@ -0,0 +1,11 @@
+package com.creditsapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Windows
+ * Date: 07.10.14
+ * Time: 21:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dttt {
+}
